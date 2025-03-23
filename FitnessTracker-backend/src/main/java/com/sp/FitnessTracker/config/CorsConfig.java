5d5277b8801e5a01521ef9005e137eef24503647
Sohil <sohilqureshi030@gmail.com>
2025-03-23 16:53:52 +0530

    Add env var for url

--- FitnessTracker-backend/src/main/java/com/sp/FitnessTracker/config/CorsConfig.java
+++ FitnessTracker-backend/src/main/java/com/sp/FitnessTracker/config/CorsConfig.java
@@ -1,15 +1,20 @@
 package com.sp.FitnessTracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
